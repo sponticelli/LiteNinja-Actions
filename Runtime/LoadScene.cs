2785846f1bbf119003663a3c94e98e08b14ee438
sponticelli <sandro.ponticelli@gmail.com>
2022-11-05 11:36:40 +0100

    feat: LoadScene allows to load duplicate scenes

--- Runtime/LoadScene.cs
+++ Runtime/LoadScene.cs
@@ -10,33 +10,44 @@ namespace LiteNinja.Actions
   {
     [SerializeField] private StringRef _sceneName;
     [SerializeField] private bool _isAdditive;
+    [SerializeField] private bool _allowDuplicate;
     [SerializeField] private bool _unloadCurrentScene;
+    [SerializeField] private bool _setAsActiveScene = true;
     [SerializeField] private float _delay = 0f;
-    
-    
+
+
     public void Load()
     {
-      if (_delay == 0)
-      {
-        Execute();
-      }
-      else
-      {
-        StartCoroutine(DelayedExecute());
-      }
-      
+      StartCoroutine(Execute());
     }
     
-    private IEnumerator DelayedExecute()
+
+
+    private IEnumerator Execute()
     {
       yield return new WaitForSeconds(_delay);
-      Execute();
-    }
+      
+      var currentScene = SceneManager.GetActiveScene();
+      var alreadyLoaded = SceneManager.GetSceneByName(_sceneName).isLoaded;
+      var loadSceneMode = _isAdditive ? LoadSceneMode.Additive : LoadSceneMode.Single;
+      
+      if (_unloadCurrentScene)
+      {
+        if (!_allowDuplicate && currentScene.name == _sceneName)
+        {
+          alreadyLoaded = false;
+        }
+        SceneManager.UnloadSceneAsync(currentScene);
+      }
+      
+      if (_allowDuplicate || !alreadyLoaded)
+      {
+        var asyncOperation = SceneManager.LoadSceneAsync(_sceneName, loadSceneMode);
+        asyncOperation.allowSceneActivation = true;
+        yield return asyncOperation;
+      }
+      if (_setAsActiveScene) SceneManager.SetActiveScene(SceneManager.GetSceneByName(_sceneName));
 
-    private void Execute()
-    {
-      if (_unloadCurrentScene) SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
-      SceneManager.LoadScene(_sceneName.Value, _isAdditive ? LoadSceneMode.Additive : LoadSceneMode.Single);
     }
   }
 }
